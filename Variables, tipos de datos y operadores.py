cfb7f9a96940f9e68e0f4d9cfc55d2ca6affa58f
TdRuiz <tdruizsamo.tech@gmail.com>
2025-11-05 20:21:49 -0400

    Variables, tipos de datos

---
+++ Variables, tipos de datos y operadores.py
@@ -0,0 +1,19 @@
+#A. Presentándome
+n = input()
+print("Hola, mi nombre es", n)
+#B. Calculando Costos
+t,a,h = map(int,input().split())
+print(t+a+h)
+#C. Descifrando El Código
+x = int(input())
+print(x%10)
+#D. Verano divertido
+x,n=map(int,input().split())
+t = x * n
+print(t)
+#E. Migración Anual de Aves
+t1,t2,t3,t4,t5 = map(int,input().split())
+print(t1+t2+t3+t4+t5+8)
+#F. Triangulos locos
+l = int(input())
+print(l*l)
